fcd1f609c7ec716cef361b613c88f5df21d4eec2
Izze Rodrigues <isap4350@gmail.com>
2024-08-22 02:11:03 -0300

    database

--- DATABASE/blog_ps.sql
+++ DATABASE/blog_ps.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 19/08/2024 às 22:33
+-- Tempo de geração: 22/08/2024 às 07:10
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -33,9 +33,17 @@ CREATE TABLE `tb_posts` (
   `id_post` int(11) NOT NULL,
   `nm_post` varchar(150) NOT NULL,
   `img_post` varchar(200) NOT NULL,
-  `dt_post` datetime(6) NOT NULL,
+  `dt_post` date NOT NULL,
   `cd_usuario` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `tb_posts`
+--
+
+INSERT INTO `tb_posts` (`id_post`, `nm_post`, `img_post`, `dt_post`, `cd_usuario`) VALUES
+(5, 'descricao', 'imagem', '2024-08-21', 1),
+(6, 'sdifygiwgyeffshuifdihuofsdohuji', 'teste', '2024-08-13', 2);
 
 -- --------------------------------------------------------
 
@@ -56,7 +64,9 @@ CREATE TABLE `tb_usuario` (
 --
 
 INSERT INTO `tb_usuario` (`id_usuario`, `nm_usuario`, `nm_email`, `cd_senha`, `img_perfil`) VALUES
-(1, 'Arthur', 'artu@email.com', '123123', '');
+(1, 'Arthur', 'artu@email.com', '123123', ''),
+(2, 'isabelle', 'isabelle@email', '12345', 'vazia'),
+(11, 'Lucas', 'lucas@email.com', '123456', '');
 
 --
 -- Índices para tabelas despejadas
@@ -67,7 +77,7 @@ INSERT INTO `tb_usuario` (`id_usuario`, `nm_usuario`, `nm_email`, `cd_senha`, `i
 --
 ALTER TABLE `tb_posts`
   ADD PRIMARY KEY (`id_post`),
-  ADD KEY `cd_usuario` (`cd_usuario`);
+  ADD KEY `FK_post_usuario` (`cd_usuario`);
 
 --
 -- Índices de tabela `tb_usuario`
@@ -83,13 +93,13 @@ ALTER TABLE `tb_usuario`
 -- AUTO_INCREMENT de tabela `tb_posts`
 --
 ALTER TABLE `tb_posts`
-  MODIFY `id_post` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_post` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de tabela `tb_usuario`
 --
 ALTER TABLE `tb_usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- Restrições para tabelas despejadas
@@ -99,7 +109,7 @@ ALTER TABLE `tb_usuario`
 -- Restrições para tabelas `tb_posts`
 --
 ALTER TABLE `tb_posts`
-  ADD CONSTRAINT `tb_posts_ibfk_1` FOREIGN KEY (`cd_usuario`) REFERENCES `tb_usuario` (`id_usuario`);
+  ADD CONSTRAINT `FK_post_usuario` FOREIGN KEY (`cd_usuario`) REFERENCES `tb_usuario` (`id_usuario`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
